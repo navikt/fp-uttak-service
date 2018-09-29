62195da634e95fe8a692cf39da4b5d3134adf074
Jan-Kåre Solbakken <jksolbakken@gmail.com>
2018-09-29 18:02:01 +0200

    upped some dependency versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,16 +2,16 @@ val junitJupiterVersion = "5.2.0"
 val spekVersion = "1.1.5"
 val kluentVersion = "1.41"
 val khttpVersion = "0.1.0"
-val javalinVersion = "2.0.0"
-val slf4jVersion = "1.8.0-beta2"
-val jacksonVersion = "2.9.6"
+val javalinVersion = "2.2.0"
+val slf4jVersion = "1.7.25"
+val jacksonVersion = "2.9.7"
 val ruleVersion = "1.2_20180904115611_cec846f"
 
 val mainClass = "no.nav.foreldrepenger.AppKt"
 
 plugins {
    application
-   kotlin("jvm") version "1.2.61"
+   kotlin("jvm") version "1.2.71"
 }
 
 buildscript {
@@ -64,7 +64,7 @@ tasks.withType<Test> {
 }
 
 tasks.withType<Wrapper> {
-   gradleVersion = "4.10"
+   gradleVersion = "4.10.2"
 }
 
 val fatJar = task("fatJar", type = Jar::class) {
