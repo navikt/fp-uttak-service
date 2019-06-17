cf8a2cef8953900b4dcd5636ac23312f879dbdfd
Andreas Dybdahl <andreas.dyb@gmail.com>
2019-06-17 15:49:31 +0200

    Nok et forsøk

--- src/test/kotlin/no/nav/foreldrepenger/KontoControllerSpec.kt
+++ src/test/kotlin/no/nav/foreldrepenger/KontoControllerSpec.kt
@@ -16,9 +16,41 @@ object KontoControllerSpec : Spek({
       given ("startdato uttak is known") {
          on("startdato uttak is before jan 1 2019") {
             it("chooses the old law") {
-               assertEquals(0, 0)
+               val familiehendelsesdato = LocalDate.of(2019, 1, 1)
+               val startdatoUttak = LocalDate.of(2018, 12, 31)
+               val chosenLaw = OLD_LAW
+               chosenLaw `should equal` OLD_LAW
+            }
+         }
+
+         on("startdato uttak is after jan 1 2019") {
+            it("chooses the new law") {
+               val familiehendelsesdato = LocalDate.of(2018, 12, 31)
+               val startdatoUttak = LocalDate.of(2019, 1, 1)
+               val chosenLaw = NEW_LAW
+               chosenLaw `should equal` NEW_LAW
             }
          }
       }
+
+      given ("startdato uttak is unknown") {
+         on("familiehendelsesdato is before jan 1 2019") {
+            it("chooses the old law") {
+               val familiehendelsesdato = LocalDate.of(2018, 12, 31)
+               val chosenLaw = OLD_LAW
+               chosenLaw `should equal` OLD_LAW
+            }
+         }
+
+         on("familiehendelsesdato is after jan 1 2019") {
+            it("chooses the new law") {
+               val familiehendelsesdato = LocalDate.of(2019, 1, 1)
+               val chosenLaw = NEW_LAW
+               chosenLaw `should equal` NEW_LAW
+            }
+         }
+      }
+
    }
+
 })
